209890c683e938d0b3a7e92f59ea2f91ee591300
Josh Matthews <jm@scrug.net>
2010-09-15 18:00:24 -0500

    Clean project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timeclock::Application.config.secret_token = 'b67bf90008f3344d4805f80ce0f2217ea097af2363bedaf94821224c5bc74e168e80f9bda96c037e4216ab6594f3784b64182e08e8a43fb643376b3f689f0a32'
