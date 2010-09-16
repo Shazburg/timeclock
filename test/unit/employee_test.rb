73a09371c15042915fb0f54b4cc60d3e96dc9a77
Josh Matthews <jm@scrug.net>
2010-09-15 21:28:50 -0500

    Testing validation

--- test/unit/employee_test.rb
+++ test/unit/employee_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class EmployeeTest < ActiveSupport::TestCase
-  def test_should_be_valid
-    assert Employee.new.valid?
+  def test_should_be_invalid
+    assert_equal false, Employee.new.valid?
+  end
+  
+  def test_validation_for_first_name
+    emp = Employee.new(:last_name => "Doe")
+    assert_equal false, emp.valid?
+  end
+  
+  def test_validation_for_last_name
+    emp = Employee.new(:first_name => "John")
+    assert_equal false, emp.valid?
   end
 end
